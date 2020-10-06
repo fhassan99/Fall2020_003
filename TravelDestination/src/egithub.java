c819a581ffc3930b895e3013f7129d8b0886aa1e
Tuan Nguyen <Tuan Nguyen@DESKTOP-8ARHKK5.austin.rr.com>
2020-10-06 15:35:24 -0500

    new branch added

--- TravelDestination/src/egithub.java
+++ TravelDestination/src/egithub.java
@@ -4,6 +4,7 @@ public class egithub {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("New line added");
+		System.out.println("create new branch");
 	}
 
 }
